73b18fa408ff28719a6aac4de451f2f91d0e6920
whgmltn15 <64678713+whgmltn15@users.noreply.github.com>
2020-05-02 16:48:36 +0900

    Add files via upload

---
+++ basic_numbers.py
@@ -0,0 +1,24 @@
+a = int(input("Enter the number a: "))
+b = int(input("Enter the number b: "))
+c = int(input())
+'''
+if  a % 2 == 0:
+    print("짝수")
+
+else:
+    print("홀수")
+'''
+ if c == 1:
+    print(a + b)
+
+ elif c == 2:
+    print(a - b)
+
+ elif c == 3:
+    print(a * b)
+
+ elif c == 4:
+    print(a / b)
+
+ elif c == 5:
+    print(a % b)
